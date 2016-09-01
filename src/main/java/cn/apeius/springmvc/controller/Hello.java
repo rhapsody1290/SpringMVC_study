187ebd3212fb69a1b491512fd33bee31d80027a8
rhapsody1290 <369201191@qq.com>
2016-09-01 21:32:10 +0800

    第一个springmvc程序

---
+++ src/main/java/cn/apeius/springmvc/controller/Hello.java
@@ -0,0 +1,21 @@
+package cn.apeius.springmvc.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Asus on 2016/9/1.
+ */
+public class Hello implements Controller{
+
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ModelAndView mv = new ModelAndView();
+        //设置试图名称
+        mv.setViewName("hello");
+        mv.addObject("msg","这是第一个springmvc程序");
+        return mv;
+    }
+}
